d1aa9451594172ab5bc45816abe518ab4f335e4c
evan-best <evan.best4@gmail.com>
2026-01-08 23:20:15 -0330

    start CategoryService

---
+++ Market/Services/CategoryService.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryService.swift
+//  Market
+//
+//  Created by Evan Best on 2026-01-08.
+//
+
+import Foundation
+
+/// Service for getting category data.
+final class CategoryService {
+	private let apiClient: APIClient
+	
+	init(apiClient: APIClient) {
+		self.apiClient = apiClient
+	}
+	
+	/// Get all categories
+	/// - Returns: An array of `Category` models.
+	func getCategories() async throws -> [Category] {
+		let endpoint = Endpoint(
+			path: "categories"
+		)
+		// TODO: Trim weird test categories.
+		return try await apiClient.get(endpoint)
+	}
+}
