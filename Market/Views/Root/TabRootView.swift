94a00f3dc4864cddfa6cd0da85c3d8f6fa1be9e0
evan-best <evan.best4@gmail.com>
2026-01-11 21:25:14 -0330

    custom icons that are cooler

--- Market/Views/Root/TabRootView.swift
+++ Market/Views/Root/TabRootView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 enum TabIdentifier: Hashable, CaseIterable {
-    case home, favorites, cart, search
+    case home, saved, bag, profile, search
 }
 
 struct TabRootView: View {
@@ -17,46 +17,29 @@ struct TabRootView: View {
 	
 	var body: some View {
 		TabView(selection: $selection) {
-			Tab(value: TabIdentifier.home) {
+			Tab("Home", image: "home", value: .home) {
 				NavigationStack {
 					HomeView()
 				}
-			} label: {
-				Image(systemName: selection == .home ? "house.fill" : "house")
-					.environment(\.symbolVariants, .none)
-				Text("Home")
 			}
-			Tab(value: TabIdentifier.favorites) {
+			
+			Tab("Favorites", image: "saved", value: .saved) {
 				NavigationStack {
 					FavoritesView()
 				}
-			} label: {
-				Image(systemName: selection == .favorites ? "heart.fill" : "heart")
-					.environment(\.symbolVariants, .none)
-				Text("Favorites")
 			}
-			Tab(value: TabIdentifier.cart) {
+			
+			Tab("Bag", image: "bag", value: .bag) {
 				NavigationStack {
 					CartView()
 				}
-			} label: {
-				Image(systemName: selection == .cart ? "cart.fill" :"cart")
-					.environment(\.symbolVariants, .none)
-				Text("Cart")
 			}
 			
-			Tab(value: TabIdentifier.search, role: .search) {
+			Tab("Search", image: "search", value: .search, role: .search) {
 				NavigationStack {
 					SearchView()
 				}
-			} label: {
-				Image(systemName: "magnifyingglass")
-					.environment(\.symbolVariants, .none)
-				Text("Search")
 			}
-		}
-		.onChange(of: selection) { newValue in
-			print("Selected: \(newValue)")
 		}
 	}
 }
